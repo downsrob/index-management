f7dcf4914ae843596b58cac466f91ff5c40ce077
Angie Zhang <langelzh@amazon.com>
2022-08-01 18:07:36 -0700

    Support query_string queries for rollup search
    Signed-off-by: Angie Zhang <langelzh@amazon.com>

--- src/main/kotlin/org/opensearch/indexmanagement/rollup/interceptor/RollupInterceptor.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/rollup/interceptor/RollupInterceptor.kt
@@ -15,6 +15,7 @@ import org.opensearch.index.query.DisMaxQueryBuilder
 import org.opensearch.index.query.MatchAllQueryBuilder
 import org.opensearch.index.query.MatchPhraseQueryBuilder
 import org.opensearch.index.query.QueryBuilder
+import org.opensearch.index.query.QueryStringQueryBuilder
 import org.opensearch.index.query.RangeQueryBuilder
 import org.opensearch.index.query.TermQueryBuilder
 import org.opensearch.index.query.TermsQueryBuilder
@@ -210,8 +211,11 @@ class RollupInterceptor(
                 }
                 fieldMappings.add(RollupFieldMapping(RollupFieldMapping.Companion.FieldType.DIMENSION, query.fieldName(), Dimension.Type.TERMS.type))
             }
+            is QueryStringQueryBuilder -> {
+                // parse the query
+            }
             else -> {
-                throw IllegalArgumentException("The ${query.name} query is currently not supported in rollups")
+                throw IllegalArgumentException("The ${query.name} query is currently not supported for rollup search.")
             }
         }
 
